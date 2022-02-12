455fbb4dd54b06c3d5d272118a335a23c8bfa284
NeginSal <neginsaljooghi@gmail.com>
2022-02-12 19:19:41 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button>New Game</button>
+    </div>
+  );
+}
+
+export default App;
