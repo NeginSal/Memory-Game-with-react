31d6e1c20511fd4e1fd8b41540c8bb6d28fa1136
NeginSal <neginsaljooghi@gmail.com>
2022-02-21 18:36:28 +0330

    match cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import "./App.css";
 import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-  { src: "/img/helmet-1.png" },
-  { src: "/img/potion-1.png" },
-  { src: "/img/ring-1.png" },
-  { src: "/img/scroll-1.png" },
-  { src: "/img/shield-1.png" },
-  { src: "/img/sword-1.png" },
+  { src: "/img/helmet-1.png" ,matched:false},
+  { src: "/img/potion-1.png" ,matched:false},
+  { src: "/img/ring-1.png" ,matched:false},
+  { src: "/img/scroll-1.png" ,matched:false},
+  { src: "/img/shield-1.png",matched:false },
+  { src: "/img/sword-1.png" ,matched:false},
 ];
 
 function App() {
@@ -37,7 +37,15 @@ function App() {
   useEffect(() => {
     if (choiceOne && choiceTwo) {
       if (choiceOne.src === choiceTwo.src) {
-        console.log("those cards match");
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
         resetTurn();
       } else {
         console.log("those cards do not match");
